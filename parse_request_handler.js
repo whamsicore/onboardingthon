7849ab47401089d7931456f208ca3c2febba7d3d
Payton Lee <paytonlee@Paytons-MacBook-Pro.local>
2016-06-30 22:14:11 -0700

    (feature) small updates, and also added nodemon

--- parse_request_handler.js
+++ parse_request_handler.js
@@ -8,13 +8,12 @@ var jsdom = require('jsdom');
 var handler = function (req, res) {
   console.log('@parse_carters endpoint');
   // console.log('@parse_carters req.body = ', req.body);
-  var $ = null;
   var html = req.body; 
 
   jsdom.env(
     html,
     function (err, window) {
-      $ = require('jquery')(window);
+      var $ = require('jquery')(window);
       // console.log('testing body string = ', $('body').text().split());
       // console.log('-------------------------------------------');
       // console.log('-------------------------------------------');
--- parsers/main.js
+++ parsers/main.js
@@ -61,3 +61,54 @@ exports.carters = function($) {
   return res_obj; 
 
 } // carters 
+
+
+
+///////////
+// CROCS //
+///////////
+exports.crocs = function($) {
+
+  /////////////
+  // ORDERNUMBER //
+  /////////////
+  try{
+      var order_number = $('body').text().match(/order number is: ([^\.\s]+)/)[1] ||
+          $('body').text().match(/order #: ([^\.\s]+)/)[1];
+  }catch(err){}
+
+  try{
+      var order_date = $('body').text().replace(/(?:\r\n|\r|\n)/g, ' ').match(/date ordered:\s+([^\s]+)/)[1];
+  }catch(err){}
+
+  /////////////
+  // PRODUCT //
+  /////////////
+  var products = [];
+  var rows = $('img[width=151]'); 
+
+  rows.get().forEach(function(item){
+    var image_url = $(item).eq(0).attr('src');
+    var attr_arr = $(item).closest('table').next().text().split('\n');
+    
+    var attr_obj = {
+        image_url: image_url, 
+        title: attr_arr[2], 
+        size: attr_arr[20], 
+        color: attr_arr[15], 
+        quantity: $(item).parents('table').eq(2).find('td.em_col2').html(),
+        price: $(item).parents('table').eq(2).find('td.em_col3').html().split('\n')[3]
+    }
+    products.push(attr_obj);
+  }) // loop
+
+  // console.log('order_number = ', order_number);
+  // console.log('products = ', products);
+  var res_obj = {
+    order_id: order_number,
+    order_date: order_date,
+    linc_care_orderitems: products
+  }
+  
+  return res_obj; 
+} // crocs
