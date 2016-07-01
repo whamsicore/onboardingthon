0a1428599425e9fab1d53b9feaf1dfa11aff373b
Payton Lee <paytonlee@Paytons-MacBook-Pro.local>
2016-06-30 22:38:07 -0700

    (refactor) parser to separate files

--- parse_request_handler.js
+++ parse_request_handler.js
@@ -1,27 +1,21 @@
-var exports = module.exports; 
+/////////////////////
+// REQUEST HANDLER //
+/////////////////////
 
 // Load Jquery
 var custom_parsers = require('./parsers/main');
 var jquery = require('jquery');
 var jsdom = require('jsdom');
 
-var handler = function (req, res) {
-  console.log('@parse_carters endpoint');
-  // console.log('@parse_carters req.body = ', req.body);
+exports.handler = function (req, res) {
   var html = req.body; 
-
+  
   jsdom.env(
     html,
     function (err, window) {
       var $ = require('jquery')(window);
-      // console.log('testing body string = ', $('body').text().split());
-      // console.log('-------------------------------------------');
-      // console.log('-------------------------------------------');
-
       var franchise = req.params.franchise;
       console.log('test, franchise = ', franchise);
       res.send(custom_parsers[franchise]($));
   }); // jsdom.env
 } // handler
-
-exports.handler = handler;
